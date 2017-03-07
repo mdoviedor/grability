3100bf190f59d11a34f2265b92b0b01167b40993
Marlon Oviedo <marlon.oviedo5@gmail.com>
2017-03-07 00:32:56 -0500

    Advance PublicGeneratorController CubeSummation

---
+++ resources/views/cube_summation/public_generator/generate.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.public')
+
+@section('content')
+{!! Form::open(['method' => 'POST', 'route' => 'cube_summation.public_generator.execute']) !!}
+
+{!! Field::textarea('cases') !!}                    
+
+<div class="form-group">
+    <div class="col-md-6 col-md-offset-4">
+        <button type="submit" class="btn btn-primary">
+            Generar
+        </button>
+    </div>
+</div>
+{!! Form::close() !!}
+@endsection
+
