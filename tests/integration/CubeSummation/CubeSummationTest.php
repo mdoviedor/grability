652847cc8bcf7d36e5b1a1a3aa2ee7d85f3d9014
Marlon Oviedo <marlon.oviedo5@gmail.com>
2017-03-06 23:05:46 -0500

    integrations tests

--- tests/integration/CubeSummation/CubeSummationTest.php
+++ tests/integration/CubeSummation/CubeSummationTest.php
@@ -3,6 +3,7 @@
 use App\Http\Lib\CubeSummation\CubeSummation;
 use App\Http\Lib\CubeSummation\DataPlainDriver;
 use App\Http\Lib\CubeSummation\exceptions\ErrorFormatInfoOperationException;
+use App\Http\Lib\CubeSummation\exceptions\ErrorInOperationException;
 
 /**
  * Description of CubeSummationTest.
@@ -56,6 +57,50 @@ QUERY 2 2 2 2 2 2';
         $cubeSummation = new CubeSummation($dataPlainDriver);
 
         $this->expectException(ErrorFormatInfoOperationException::class);
+        $cubeSummation->get();
+    }
+
+    public function test_run_operation_with_diferent_type_case_1()
+    {
+        $data = '2
+4 5
+RESET 2 2 2 4
+QUERY 1 1 1 3 3 3
+UPDATE 1 1 1 23
+QUERY 2 2 2 4 4 4
+QUERY 1 1 1 3 3 3
+2 4
+UPDATE 2 2 2 1
+QUERY 1 1 1 1 1 1
+QUERY 1 1 1 2 2 2
+QUERY 2 2 2 2 2 2';
+
+        $dataPlainDriver = new DataPlainDriver($data);
+        $cubeSummation = new CubeSummation($dataPlainDriver);
+
+        $this->expectException(ErrorFormatInfoOperationException::class);
+        $cubeSummation->get();
+    }
+
+    public function test_run_operation_update_x_greate_than_matrix()
+    {
+        $data = '2
+4 5
+UPDATE 100 2 2 4
+QUERY 1 1 1 3 3 3
+UPDATE 1 1 1 23
+QUERY 2 2 2 4 4 4
+QUERY 1 1 1 3 3 3
+2 4
+UPDATE 2 2 2 1
+QUERY 1 1 1 1 1 1
+QUERY 1 1 1 2 2 2
+QUERY 2 2 2 2 2 2';
+
+        $dataPlainDriver = new DataPlainDriver($data);
+        $cubeSummation = new CubeSummation($dataPlainDriver);
+
+        $this->expectException(ErrorInOperationException::class);
         $cubeSummation->get();
     }
 }
